d2ec6883e8bdbecce65948e503f391cbc17d864a
SandaliSewmini <sandalikaru123@gmail.com>
2026-04-24 02:09:16 +0530

    database

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp, getApps } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDAFSgFeYvjZz3Un0_8iaTM7BgAF7wymwk",
-  authDomain: "fire-monitoring-system-e0752.firebaseapp.com",
+  apiKey: "AIzaSyCpo90DE6V6fh4ToTl69ipvi4dhek76KLA",
+  authDomain: "iotproject-b12b4.firebaseapp.com",
   databaseURL:
-    "https://fire-monitoring-system-e0752-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "fire-monitoring-system-e0752",
-  storageBucket: "fire-monitoring-system-e0752.appspot.com",
-  messagingSenderId: "212281652347",
-  appId: "1:212281652347:web:944cd1774802f7eb155ede",
+    "https://iotproject-b12b4-default-rtdb.firebaseio.com",
+  projectId: "iotproject-b12b4",
+  storageBucket: "iotproject-b12b4.firebasestorage.app",
+  messagingSenderId: "254397755394",
+  appId: "1:254397755394:web:c782ea3214f09dd1f2371b",
 };
 
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
